49db2f83bd89f6ee0b8b46c0f69beb7ef534cf12
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-06-10 12:43:34 -0400

    add review routes

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const postsRouter = require('./routes/posts')
+const postsRouter = require('./routes/posts');
+const reviewsRouter = require('./routes/reviews');
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
+app.use('/post/:id/reviews', reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
