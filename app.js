2d126caea4d1d73c06ec87e07214410526c09f25
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-06-11 09:42:07 -0400

    update routes, connec to MongoDB, add full data to model on post request

--- app.js
+++ app.js
@@ -4,23 +4,51 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const User = require('./models/user');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
+
+// require routes
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
 
 const app = express();
 
+//connect to the database
+mongoose.connect('mongodb://localhost:27017/coffee-shop', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("we're connected!");
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: false } ));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// configure Passport and Sessions
+app.use(session({
+  secret: 'cold brew please',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// mount routes
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/post/:id/reviews', reviewsRouter);
